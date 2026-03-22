084be2ab103896ffaccad15a242c51cdbcfab038
katttanagatari <b3sh1k@gmail.com>
2026-03-22 14:24:03 +0400

    feat(main): refactoring everything

--- content.js
+++ content.js
@@ -13,8 +13,8 @@
     slider: {
       min: 0, // min volume value
       max: 1, // max volume value
-      step: 0.01,   // value change step 
-      defaultVolume: 0.5,   // def volume
+      step: 0.01, // value change step 
+      defaultVolume: 0.5, // def volume
       styles: {
         base: // styles for showing the slider
         `
@@ -41,14 +41,16 @@
    * Initialize Volume Controller
   **/
   function init() {
-    const targetElement = document.querySelector(CONFIG.selectors.target);  // main element selector
-    const remoteMedia = document.getElementById(CONFIG.selectors.remoteMedia.replace('#', '')); // audio selector
+    const targetEl = document.querySelector(CONFIG.selectors.target);  // main element selector
+    const remoteMediaEl = document.getElementById(CONFIG.selectors.remoteMedia.replace('#', '')); // audio selector
 
-    if (!targetElement || !remoteMedia) { // until find targetElement
+    if (!targetEl || !remoteMediaEl) { // until find targetElement
       return setTimeout(init, CONFIG.retryDelay);
     }
-
-    createVolumeInterface(targetElement, remoteMedia); // create interface
+    const ui = createUI(targetEl);
+    createEvents(ui, remoteMediaEl);
+    
+    // createVolumeInterface(targetEl, remoteMediaEl); // create interface
   }
 
   /**
@@ -57,22 +59,145 @@
     * @param {HTMLElement} remoteMedia - element to control volume
   **/
 
-  function createVolumeInterface(targetElement, remoteMedia) {
+  // function createVolumeInterface(targetElement, remoteMedia) {
 
-    const container = document.createElement('div');  // container 
-    const containerForInput = document.createElement('div');  // container for slider 
-    const slider = document.createElement('input'); // slider
-    const newButton = document.createElement('button'); // mute,unmute btn
+  //   // const container = document.createElement('div');  // container 
+  //   // const containerForInput = document.createElement('div');  // container for slider 
+  //   // const slider = document.createElement('input'); // slider
+  //   // const newButton = document.createElement('button'); // mute,unmute btn
+  //   const [container, containerForInput, slider, newButton] = createUI(targetEl);
 
 
-    container.style.cssText = // container's style
+  //   container.style.cssText = // container's style
+  //   ` 
+  //     position: relative;
+  //     display: inline-block;
+  //     margin-right: 8px;
+  //   `;
+
+  //   containerForInput.style.cssText = // container for slider's style 
+  //   `
+  //     position: absolute;
+  //     bottom: 100%;
+  //     left: 50%;
+  //     transform: translateX(-50%);
+  //     width: 60px;
+  //     height: 140px;
+  //     display: flex;
+  //     justify-content: center;
+  //     align-items: center;
+  //     z-index: 1000;
+  //   `;
+
+  //   slider.type = 'range';  // сonfigure slider
+  //   slider.min = CONFIG.slider.min;
+  //   slider.max = CONFIG.slider.max;
+  //   slider.step = CONFIG.slider.step;
+  //   slider.value = CONFIG.slider.defaultVolume;
+  //   slider.style.cssText = CONFIG.slider.styles.hidden;
+
+  //   newButton.innerHTML = // button mute with icon
+  //   `
+  //     <span class="sc-hGPBjI jkgjts">
+  //       <i class="sc-bdvvtL goIptw icon-bbb-volume_up"></i>
+  //     </span>
+  //   `;
+  //   newButton.className = 'sc-dJjYzT jeAcue lg buttonWrapper buttonSound';
+
+
+  //   container.appendChild(containerForInput); // create dom structure
+  //   containerForInput.appendChild(slider);
+  //   container.appendChild(newButton);
+  //   targetElement.parentNode.insertBefore(container, targetElement);
+
+  //   /**
+  //    * Volume change handler
+  //    * @event slider#input
+  //   **/
+////////////////////////////////////////////////////
+
+
+
+  //   slider.addEventListener('input', () => {
+  //     if (remoteMedia) {
+  //       remoteMedia.volume = slider.value;  // set volume on handler
+  //       console.log(remoteMedia.volume)
+  //       updateVolumeIcon(newButton, slider.value);  // update icon
+  //     }
+  //   });
+
+  //   /**
+  //    * Mute change btn
+  //    * @event newButton#click
+  //   **/
+
+  //   let isMuted = false; // flag
+  //   let tempSliderPosition = slider.value; // temp slider value
+
+  //   newButton.addEventListener('click', () => {
+  //     if (!remoteMedia) return;
+
+  //     isMuted = !isMuted;
+  //     remoteMedia.muted = isMuted;
+
+  //     if (isMuted) { // save position while muted by click
+  //       tempSliderPosition = slider.value;
+  //       slider.value = 0;
+  //     } else {
+  //       slider.value = tempSliderPosition;
+  //       remoteMedia.volume = tempSliderPosition;
+  //     }
+
+  //     updateVolumeIcon(newButton, slider.value);   // update icon
+  //   });
+
+  //   /**
+  //    * Hover events
+  //    * @event containerForInput,newButton#mouseover,mouseout
+  //   **/
+
+  //   [containerForInput, newButton].forEach(el => {
+  //     el.addEventListener('mouseover', () => {
+  //       slider.style.cssText = CONFIG.slider.styles.base;
+  //     });
+
+  //     el.addEventListener('mouseout', () => {
+  //       slider.style.cssText = CONFIG.slider.styles.hidden;
+  //     });
+  //   });
+
+  //   console.log('everything works fine)');
+  // }
+
+  function createUI(targetEl){
+    const container = createContainer();
+    const sliderWrapper = createForInput();
+    const slider = createSlider();
+    const btn =  createBtn();
+
+    container.appendChild(sliderWrapper);
+    sliderWrapper.appendChild(slider);
+    container.appendChild(btn);
+    targetEl.parentNode.insertBefore(container, targetEl);
+
+    return {container, sliderWrapper, slider, btn};
+  };
+
+  function createContainer(){
+    const el = document.createElement('div');
+    el.style.cssText = // container's style
     ` 
       position: relative;
       display: inline-block;
       margin-right: 8px;
     `;
 
-    containerForInput.style.cssText = // container for slider's style 
+    return el;
+  };
+
+  function createForInput(){
+    const el = document.createElement('div');
+    el.style.cssText = // container for slider's style 
     `
       position: absolute;
       bottom: 100%;
@@ -86,95 +211,73 @@
       z-index: 1000;
     `;
 
-    slider.type = 'range';  // сonfigure slider
-    slider.min = CONFIG.slider.min;
-    slider.max = CONFIG.slider.max;
-    slider.step = CONFIG.slider.step;
-    slider.value = CONFIG.slider.defaultVolume;
-    slider.style.cssText = CONFIG.slider.styles.hidden;
+    return el;
+  };
 
-    newButton.innerHTML = // button mute with icon
+  function createSlider(){
+    const el = document.createElement('input');
+    el.type = 'range';
+    el.min = CONFIG.slider.min;
+    el.max = CONFIG.slider.max;
+    el.step = CONFIG.slider.step;
+    el.value = CONFIG.slider.defaultVolume;
+    el.style.cssText = CONFIG.slider.styles.hidden;
+
+    return el;
+  };
+
+  function createBtn(){
+    const el = document.createElement('button');
+    el.innerHTML = // button mute with icon
     `
       <span class="sc-hGPBjI jkgjts">
         <i class="sc-bdvvtL goIptw icon-bbb-volume_up"></i>
       </span>
     `;
-    newButton.className = 'sc-dJjYzT jeAcue lg buttonWrapper buttonSound';
+    el.className = 'sc-dJjYzT jeAcue lg buttonWrapper buttonSound';
 
+    return el;
+  };
 
-    container.appendChild(containerForInput); // create dom structure
-    containerForInput.appendChild(slider);
-    container.appendChild(newButton);
-    targetElement.parentNode.insertBefore(container, targetElement);
+  function createEvents(ui,remoteMedia){
+    changeVolume(ui,remoteMedia);
 
-    /**
-     * Volume change handler
-     * @event slider#input
-    **/
+  }
 
-    slider.addEventListener('input', () => {
+  function changeVolume(ui,remoteMedia){
+    ui.slider.addEventListener('input', () => {
       if (remoteMedia) {
-        remoteMedia.volume = slider.value;  // set volume on handler
-        updateVolumeIcon(newButton, slider.value);  // update icon
+        remoteMedia.volume = ui.slider.value;  // set volume on handler
+        console.log(remoteMedia.volume)
+        updateVolumeIcon(ui.btn, ui.slider.value);  // update icon
       }
     });
+  }
 
-    /**
-     * Mute change btn
-     * @event newButton#click
-    **/
 
-    let isMuted = false; // flag
-    let tempSliderPosition = slider.value; // temp slider value
-
-    newButton.addEventListener('click', () => {
-      if (!remoteMedia) return;
-
-      isMuted = !isMuted;
-      remoteMedia.muted = isMuted;
-
-      if (isMuted) { // save position while muted by click
-        tempSliderPosition = slider.value;
-        slider.value = 0;
-      } else {
-        slider.value = tempSliderPosition;
-        remoteMedia.volume = tempSliderPosition;
-      }
-
-      updateVolumeIcon(newButton, slider.value);   // update icon
-    });
-
-    /**
-     * Hover events
-     * @event containerForInput,newButton#mouseover,mouseout
-    **/
-
-    [containerForInput, newButton].forEach(el => {
-      el.addEventListener('mouseover', () => {
-        slider.style.cssText = CONFIG.slider.styles.base;
-      });
-
-      el.addEventListener('mouseout', () => {
-        slider.style.cssText = CONFIG.slider.styles.hidden;
-      });
-    });
-
-    /**
-     * Update icon volume
-     * @param {HTMLElement} button - button for update icon
-     * @param {integer} volume - volume value
-    **/
-
-    function updateVolumeIcon(button, volume) {
-      const icon = button.querySelector('i');
+  /**
+    * Update icon volume
+    * @param {HTMLElement} button - button for update icon
+    * @param {integer} volume - volume value
+  **/
+   
+  function updateVolumeIcon(button, volume) {
+    const icon = button.querySelector('i');
       if (!icon) return;
 
       const volumeNum = parseFloat(volume);
       icon.className = volumeNum > 0 
         ? 'sc-bdvvtL goIptw icon-bbb-volume_up'
         : 'sc-bdvvtL goIptw icon-bbb-volume_off';
-    }
-    console.log('everything works fine)');
-  }
-    init();
+  };
+
+  // const targetEl = document.querySelector('body');
+  // const ui = createUI(targetEl);
+  // console.log(test)
+  // setTimeout(() => {
+  //   targetEl.appendChild(test);
+  //   console.log('123')
+  // }, 5000);
+
+  init();
 })();
